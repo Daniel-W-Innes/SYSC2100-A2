04f4ff337fb6f3a46141dadc485754f60045d130
Daniel Innes <Daniel.W.Innes@gmail.com>
2019-02-07 21:35:18 -0500

    JavaDocs

---
+++ Assignement 2/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CountSubstrings"},{"p":"<Unnamed>","l":"CountSubstrings.Result"},{"p":"<Unnamed>","l":"CountSubstrings.UserInput"}]
--- Assignement 2/src/CountSubstrings.java
+++ Assignement 2/src/CountSubstrings.java
@@ -2,6 +2,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+/**
+ * Count occurrences of substring in texts using various different implementations of the ADT List and timing it.
+ * @author Daniel Innes 101067175
+ */
+
 public class CountSubstrings {
     public static void main(String[] args) {
 
@@ -16,13 +21,21 @@ public class CountSubstrings {
             e.printStackTrace();
         }
 
-        HashMap<String, Result> results = averageResults(texts, userInput.getSubstring(),1000);
+        HashMap<String, Result> results = averageResults(texts, userInput.getSubstring());
 
         for (String key : results.keySet()) {
             Result result = results.get(key);
             System.out.println("Using " + key + ": " + result.getCount() + " matches, derived in " + result.getDeltaTime() + " milliseconds.");
         }
     }
+
+    /**
+     * Count occurrences of substring in all texts and time it.
+     * @param texts HashMap of the texts using various different implementations of the ADT List. Key is the name of implementation and the List of List of Characters.
+     * @param substring A List of Characters to find in the List of List of Characters.
+     * @return A custom helper class (Result) to pass deltaTime and count for each implementation.
+     * @see Result
+     */
 
     private static HashMap<String, Result> countOccurrences(HashMap<String, List<List<Character>>> texts, List<Character> substring) {
         long time;
@@ -41,10 +54,17 @@ public class CountSubstrings {
         return Results;
     }
 
-    private static HashMap<String, Result> averageResults(HashMap<String, List<List<Character>>> texts, List<Character> substring, int number) {
+    /**
+     * Average over 1000 of Count occurrences of substring in all texts and time it.
+     * @param texts HashMap of the texts using various different implementations of the ADT List. Key is the name of implementation and the List of List of Characters.
+     * @param substring A List of Characters to find in the List of List of Characters.
+     * @return A custom helper class (Result) to pass deltaTime and count for each implementation.
+     */
+    private static HashMap<String, Result> averageResults(HashMap<String, List<List<Character>>> texts, List<Character> substring) {
+        int number = 1000;
         HashMap<String, Result> results;
-        HashMap<String, Result> resultsSum = new HashMap<String, Result>();
-        HashMap<String, Result> resultsAverage = new HashMap<String, Result>();
+        HashMap<String, Result> resultsSum = new HashMap<>();
+        HashMap<String, Result> resultsAverage = new HashMap<>();
         results = countOccurrences(texts, substring);
         for (String key :texts.keySet()) {
             resultsSum.put(key,results.get(key));
@@ -64,7 +84,10 @@ public class CountSubstrings {
         return resultsAverage;
     }
 
-
+    /**
+     * Get the fileName and pattern from console input.
+     * @return A custom helper class (Result) to pass deltaTime and count for each implementation.
+     */
     private static UserInput getUserInput() {
         Scanner scannerConsole = new Scanner(System.in);
         File file;
@@ -79,6 +102,12 @@ public class CountSubstrings {
         return new UserInput(file, substring);
     }
 
+    /**
+     * Read from file to ArrayLists parse by space.
+     * @param file The file to read from.
+     * @return The file as ArrayLists.
+     * @throws FileNotFoundException If the file dose not exist, this should never happen because file checking is done during input stage.
+     */
     private static ArrayList<List<Character>> readToArrayLists(File file) throws FileNotFoundException {
         Scanner scannerFile = new Scanner(file);
         scannerFile.useDelimiter(" ");
@@ -89,6 +118,12 @@ public class CountSubstrings {
         return text;
     }
 
+    /**
+     * Read from file to LinkedList parse by space.
+     * @param file The file to read from.
+     * @return The file as LinkedList.
+     * @throws FileNotFoundException If the file dose not exist, this should never happen because file checking is done during input stage.
+     */
     private static LinkedList<List<Character>> readToLinkedList(File file) throws FileNotFoundException {
         Scanner scannerFile = new Scanner(file);
         scannerFile.useDelimiter(" ");
@@ -99,14 +134,22 @@ public class CountSubstrings {
         return text;
     }
 
-    private static int countBrute(List<Character> text, List<Character> substring) {
+    /**
+     * Count occurrences of substring in string using the brute-force substring matching algorithm.
+     *
+     * Note findBrute is not used because it dose not find multiple occurrences of substring in each string. Therefore will be less efficient for substrings that occur multiple times within a word.
+     * @param string The string to search in.
+     * @param substring The string to search for.
+     * @return The number of substring in string.
+     */
+    private static int countBrute(List<Character> string, List<Character> substring) {
         int m = substring.size();
-        int n = text.size();
+        int n = string.size();
         int count = 0;
         for (int i = 0; i <= n - m; i++) {
             int j;
             for (j = 0; j < m; j++) {
-                if (text.get(i + j) != substring.get(j))
+                if (string.get(i + j) != substring.get(j))
                     break;
             }
             if (j == m) count++;
@@ -114,37 +157,70 @@ public class CountSubstrings {
         return count;
     }
 
-    public static class UserInput {
+
+    /**
+     * Helper class containing user input.
+     */
+    private static class UserInput {
         private final File file;
         private final List<Character> substring;
 
+        /**
+         * Initialize file and substring.
+         * @param file The file to search.
+         * @param substring The substring to search for.
+         */
         UserInput(File file, List<Character> substring) {
             this.file = file;
             this.substring = substring;
         }
 
+        /**
+         * Get the file.
+         * @return The file.
+         */
         File getFile() {
             return file;
         }
 
+        /**
+         * Get the substring.
+         * @return The substring.
+         */
         List<Character> getSubstring() {
             return substring;
         }
     }
 
-    public static class Result {
+    /**
+     * Helper class containing the result of a search.
+     */
+    private static class Result {
         private final long deltaTime;
         private final int count;
 
+        /**
+         * Initialize deltaTime and count.
+         * @param deltaTime The Time need to search the text.
+         * @param count The number of substrings in the text.
+         */
         Result(long deltaTime, int count) {
             this.deltaTime = deltaTime;
             this.count = count;
         }
 
+        /**
+         * Get the deltaTime.
+         * @return The deltaTime.
+         */
         long getDeltaTime() {
             return deltaTime;
         }
 
+        /**
+         * Get the count.
+         * @return The count.
+         */
         int getCount() {
             return count;
         }
